00c0f5094e3ca4348a9ae53668b73c64cff82625
Your Name <you@example.com>
2015-11-19 08:00:10 +0000

    change tem

--- temperature.js
+++ temperature.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 
 module.exports = {
     getTemp: function() {
-        var data = fs.readFileSync('/sys/bus/w1/devices/28-0215536f5dff/w1_slave');
+        var data = fs.readFileSync('/sys/bus/w1/devices/28-0215536f5dff/w1_slave').toString();
 
-        return data;
+        return parseInt(data.substring(data.indexOf('t=') + 2))/1000;
     }
 };
